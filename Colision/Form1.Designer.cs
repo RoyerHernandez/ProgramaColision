d5b902483f7598bd5e00199906eb4fc8eca612d5
Royer Alfonso Hernandez Morales <royer.hernandez@servientrega.com>
2019-02-13 15:07:31 -0500

    version1_New

---
+++ Colision/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace Colision
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(506, 22);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(192, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "\"\"";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Colision.Properties.Resources.CircleGreen;
+            this.pictureBox2.Location = new System.Drawing.Point(163, 77);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox1_Click_1);
+            this.pictureBox2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox2_MouseDown);
+            this.pictureBox2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox2_MouseMove);
+            this.pictureBox2.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox2_MouseUp);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Colision.Properties.Resources.CircleRed2;
+            this.pictureBox1.Location = new System.Drawing.Point(293, 77);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Colision/Form1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Colision
+{
+    public partial class Form1 : Form
+    {
+
+        private bool seMueve = false;
+        private int ofsetx;
+        private int ofsety;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+
+        private void moverArriba(object sender, MouseEventArgs e)
+        {
+            seMueve = true;
+        }
+
+
+        private void moverIzq(object sender, MouseEventArgs e)
+        {
+            seMueve = true;
+        }
+
+        private void moverDer(object sender, MouseEventArgs e)
+        {
+            seMueve = true;
+        }
+
+        private void imgMoverMouse(object sender, MouseEventArgs e)
+        {
+            Control ctr = sender as Control;
+            if (seMueve)
+            {
+                Point P1 = ctr.PointToScreen(e.Location);
+                Point P2 = ctr.Parent.PointToClient(P1);
+                ctr.Location = P2;
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            seMueve = true;
+            ofsetx = e.X;
+            ofsety = e.Y;
+        }
+
+        private void pictureBox2_MouseDown(object sender, MouseEventArgs e)
+        {
+            seMueve = true;
+            ofsetx = e.X;
+            ofsety = e.Y;
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (seMueve)
+            {
+                pictureBox1.Left += e.X - ofsetx;
+                pictureBox1.Top += e.Y - ofsety;
+            }
+        }
+
+        private void pictureBox2_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (seMueve)
+            {
+                pictureBox2.Left += e.X - ofsetx;
+                pictureBox2.Top += e.Y - ofsety;
+            }
+        }
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            int locX = 0;
+            int locY = 0;
+
+            int loc2X = 0;
+            int loc2Y = 0;
+
+            int difX;
+            int difY;
+
+            String imagePath = Path.Combine(Application.StartupPath, "E:\\Documentos\\Personal\\Especialización\\Colision\\SquareRed.jpg");            
+            String imagePath3 = Path.Combine(Application.StartupPath, "E:\\Documentos\\Personal\\Especialización\\Colision\\CircleRed2.jpg");
+
+            if (e.Button == MouseButtons.Left) {
+
+                seMueve = false;
+
+                locX = pictureBox1.Location.X + pictureBox1.Width;
+                locY = pictureBox1.Location.Y + pictureBox1.Height;
+
+                loc2X = pictureBox2.Location.X + pictureBox2.Width;
+                loc2Y = pictureBox2.Location.Y + pictureBox2.Height;
+
+                difX = locX - loc2X;
+                difY = locY - loc2Y;
+
+                if ( difX >= -100 && difX <= 100 )
+                {
+                    if (difY >= -100 && difY <= 100) { 
+                    button1.Text = "Colision !!";
+                    button1.BackColor = Color.Red;
+                    pictureBox1.Image = Image.FromFile(imagePath);
+                    }
+                    else
+                    {
+                        button1.Text = "No Hay Colision";
+                        button1.BackColor = Color.LightGreen;
+                        pictureBox1.Image = Image.FromFile(imagePath3);
+                    }
+                }
+                else
+                {
+                    button1.Text = "No Hay Colision";
+                    button1.BackColor = Color.LightGreen;
+                    pictureBox1.Image = Image.FromFile(imagePath3);
+                }
+
+            }
+        }
+
+        private void pictureBox2_MouseUp(object sender, MouseEventArgs e)
+        {
+            int locX = 0;
+            int locY = 0;
+
+            int loc2X = 0;
+            int loc2Y = 0;
+
+            int difX;
+            int difY;
+
+            String imagePath2 = Path.Combine(Application.StartupPath, "E:\\Documentos\\Personal\\Especialización\\Colision\\SquareGreen.jpg");
+            String imagePath4 = Path.Combine(Application.StartupPath, "E:\\Documentos\\Personal\\Especialización\\Colision\\CircleGreen.jpg");
+
+            if (e.Button == MouseButtons.Left)
+            {
+                seMueve = false;
+
+                locX = pictureBox1.Location.X + pictureBox1.Width;
+                locY = pictureBox1.Location.Y + pictureBox1.Height;
+
+                loc2X = pictureBox2.Location.X + pictureBox2.Width;
+                loc2Y = pictureBox2.Location.Y + pictureBox2.Height;
+
+                difX = locX - loc2X;
+                difY = locY - loc2Y;
+
+                if (difX >= -100 && difX <= 100)
+                {
+                    if (difY >= -100 && difY <= 100)
+                    {
+                        button1.Text = "Colision !!";
+                        button1.BackColor = Color.Red;
+                        pictureBox2.Image = Image.FromFile(imagePath2);
+                    }
+                    else
+                    {
+                        button1.Text = "No Hay Colision";
+                        button1.BackColor = Color.LightGreen;
+                        pictureBox2.Image = Image.FromFile(imagePath4);
+                    }
+                }
+                else
+                {
+                    button1.Text = "No Hay Colision";
+                    button1.BackColor = Color.LightGreen;
+                    pictureBox2.Image = Image.FromFile(imagePath4);
+                }
+
+            }
+        }
+
+        private void pictureBox1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
